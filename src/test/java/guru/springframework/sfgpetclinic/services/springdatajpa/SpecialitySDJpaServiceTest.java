32c1a9099dd0710b13375b6ed962d257d6566880
yulikexuan <yulikexuan@gmail.com>
2019-03-10 12:42:31 -0400

    Verify calling mocks

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -27,11 +28,70 @@ class SpecialitySDJpaServiceTest {
 	void setUp() {
 	}
 
-	@DisplayName("Verify injected field when delete by id -")
+	@DisplayName("Should be able to delete Speciality by id -")
 	@Test
 	void testDeleteById() {
+
+		// Given
+		long id = 1L;
+
 		// When
 		this.specialitySDJpaService.deleteById(1L);
+		this.specialitySDJpaService.deleteById(1L);
+
+		// Then
+		verify(this.specialtyRepository, times(2))
+				.deleteById(id);
+		verify(this.specialtyRepository, atLeastOnce()).deleteById(id);
+		verify(this.specialtyRepository, atMost(5))
+				.deleteById(id);
+	}
+
+	@DisplayName("Should be able to delete by id at least once -")
+	@Test
+	void testDeleteByIdAtLeastOnce() {
+
+		// Given
+		long id = 1L;
+
+		// When
+		this.specialitySDJpaService.deleteById(1L);
+		this.specialitySDJpaService.deleteById(1L);
+
+		// Then
+		verify(this.specialtyRepository, atLeastOnce()).deleteById(id);
+	}
+
+	@DisplayName("Should be able to delete by id at most five times -")
+	@Test
+	void testDeleteByIdAtMostFiveTimes() {
+
+		// Given
+		long id = 1L;
+
+		// When
+		this.specialitySDJpaService.deleteById(1L);
+		this.specialitySDJpaService.deleteById(1L);
+
+		// Then
+		verify(this.specialtyRepository, atMost(5))
+				.deleteById(id);
+	}
+
+	@DisplayName("Never delete by id 5 -")
+	@Test
+	void testDeleteByIdNever() {
+
+		// Given
+		long id = 1L;
+
+		// When
+		this.specialitySDJpaService.deleteById(1L);
+		this.specialitySDJpaService.deleteById(1L);
+
+		// Then
+		verify(this.specialtyRepository, never())
+				.deleteById(5L);
 	}
 
 	@DisplayName("Verify injected field when delete speciality -")
